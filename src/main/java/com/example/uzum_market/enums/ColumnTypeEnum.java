bb4f3221351f2874244e0c8c20f2b59ed7da838d
Jamshid <jamshid102002@gmail.com>
2023-11-10 22:44:02 +0500

    change1

---
+++ src/main/java/com/example/uzum_market/enums/ColumnTypeEnum.java
@@ -0,0 +1,11 @@
+package com.example.uzum_market.enums;
+
+public enum ColumnTypeEnum {
+
+    SHORT_TEXT,
+    DATE,
+    NUMBER,
+    MONEY,
+    CHECKBOX,
+    FILE
+}
---
+++ src/main/java/com/example/uzum_market/enums/ComparatorTypeEnum.java
@@ -0,0 +1,13 @@
+package com.example.uzum_market.enums;
+
+public enum ComparatorTypeEnum {
+    EQ,
+    NOT,
+    LT,
+    LTE,
+    GT,
+    GTE,
+    RA,
+    IS_SET,
+    IS_NOT_SET,
+}
